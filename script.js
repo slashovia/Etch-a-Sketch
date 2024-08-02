19f6ad2f5f0c1ff5ba92432924dd75930992902f
Hashmi <digiacomo93@gmail.com>
2024-08-02 06:40:26 +0200

    Fix addEventListener for grid element

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('button');
 
+
 for (let index = 0; index < 16 ** 2; index++) {
     const div = document.createElement('div');
+    div.classList.add("grid");
     container.appendChild(div);
 };
 
 function createGrid(input) {
-    const allDiv = document.querySelectorAll('.container div')
+    const allDiv = document.querySelectorAll('.container div');
     allDiv.forEach(element => { element.remove() });
     for (let index = 0; index < input ** 2; index++) {
         const div = document.createElement('div');
+        div.classList.add("grid");
         div.style.flexBasis = `${100 / input}%`;
         container.appendChild(div);
     };
 
 }
 
-container.addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = 'black';
+const allGrids = document.querySelectorAll('.grid');
+allGrids.forEach(grid => {
+    grid.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = `rgb(${rgbNumber()}, ${rgbNumber()}, ${rgbNumber()})`;
+    })
 });
 
+function rgbNumber() {
+    return Math.floor(Math.random() * (255 - 0 + 1) + 0);
+}
 
 button.addEventListener('click', () => {
 
